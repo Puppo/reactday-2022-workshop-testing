2f7642328a8fdcd4f8751c26a96085cdfc6b44f0
Luca Del Puppo <luca@delpuppo.net>
2022-10-08 17:34:07 +0200

    01 first example complete

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,9 +1,26 @@
-import './Search.scss'
+import { useState } from 'react';
+import './Search.scss';
 
 export default function Search() {
+  const [searchTerm, setSearchTerm] = useState('')
+  const isButtonDisabled = searchTerm.length < 3;
+
   return (
     <div className='Search'>
-      <h1>Search</h1>
+      <h1 className='title'>Search</h1>
+
+      <form className="Search-form">
+        <div className="field has-addons">
+          <div className="control">
+            <input className="input" name="search-input" type="text" placeholder="Type the username" value={searchTerm} onChange={e => setSearchTerm(e.target.value)} />
+          </div>
+          <div className="control">
+            <button type="submit" className="button is-info" disabled={isButtonDisabled}>
+              Search
+            </button>
+          </div>
+        </div>
+      </form>
     </div>
   )
 }
