62e0f541182ed4c9cb73da4f9ad97f74ef7a4b7a
Luca Del Puppo <luca@delpuppo.net>
2022-10-12 19:13:48 +0200

    revert solution

--- src/pages/Search/Search.spec.tsx
+++ src/pages/Search/Search.spec.tsx
@@ -1,7 +1,6 @@
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
-import { gitHubService } from '../../services';
 import Search from './Search';
 
 jest.mock('../../services/github.service')
@@ -32,24 +31,6 @@ describe('Search Page', () => {
 
   describe('search', () => {
 
-    let searchUsersMocked: jest.MockedFn<typeof gitHubService.searchUsers>;
-    const mockResult = {
-      items: [{
-        login: 'login-1',
-        avatar_url: 'login-1-avatar'
-      }, {
-        login: 'login-2',
-        avatar_url: 'login-2-avatar'
-      }, {
-        login: 'login-3',
-        avatar_url: 'login-3-avatar'
-      }]
-    }
-
-    beforeEach(() => {
-      searchUsersMocked = gitHubService.searchUsers as jest.MockedFn<typeof gitHubService.searchUsers>
-    })
-
     it('should enable the search button if the user type al least 3 characters', () => {
       renderSearchComponent();
 
@@ -66,53 +47,11 @@ describe('Search Page', () => {
     })
 
     describe('perform search', () => {
-      const findAllUsersLinks = () => waitFor(() => screen.findAllByRole('link'))
-      const findAllUsersImages = () => waitFor(() => screen.findAllByRole('img'))
+      it.todo('should render the users');
 
-      const setupMock = () => {
-        searchUsersMocked.mockResolvedValue(mockResult)
-      }
+      it.todo('should render for each user the username as link for the detail page');
 
-      const performSearch = () => {
-        const searchTerm = 'test';
-        renderSearchComponent();
-
-        userEvent.type(getSearchInput(), searchTerm);
-        userEvent.click(getSearchButton());
-      }
-
-      it('should render the users', async () => {
-        setupMock();
-
-        performSearch();
-
-        const resultList = await findAllUsersLinks();
-        expect(resultList.length).toBe(mockResult.items.length)
-      });
-
-      it('should render for each user the username as link for the detail page', async () => {
-        setupMock();
-
-        performSearch();
-
-        const resultList = await findAllUsersLinks()
-        resultList.forEach((el, index) => {
-          expect(el).toHaveAttribute('href', `/${mockResult.items[index].login}/repositories`,)
-        })
-      });
-
-      it('should render for each user the avatar image', async () => {
-        setupMock();
-
-        performSearch();
-
-        const resultList = await findAllUsersImages()
-        resultList.forEach((el, index) => {
-          expect(el).toHaveAttribute('src', mockResult.items[index].avatar_url)
-        })
-      });
-
-
+      it.todo('should render for each user the avatar image');
     })
   })
 
--- src/services/github.service.spec.ts
+++ src/services/github.service.spec.ts
@@ -1,71 +1,14 @@
-import { rest } from "msw";
-import { setupServer } from "msw/node";
-import { searchUsers, UserSearchResult } from "./github.service";
-
 describe("GitHubService", () => {
   describe("searchUsers", () => {
-    const mockResult = (query: string): UserSearchResult => ({
-      items: [
-        {
-          login: `${query}-1`,
-          avatar_url: `${query}-1-avatar`,
-        },
-        {
-          login: `${query}-2`,
-          avatar_url: `${query}-2-avatar`,
-        },
-        {
-          login: `${query}-3`,
-          avatar_url: `${query}-3-avatar`,
-        },
-      ],
-    });
+    it.todo("should return the correct result");
 
-    const server = setupServer(
-      // capture "GET /greeting" requests
-      rest.get(`https://api.github.com/search/users`, (req, res, ctx) => {
-        const query = req.url.searchParams.get("q");
-        if (!query) return res(ctx.status(400));
-
-        return res(ctx.json(mockResult(query)));
-      })
+    it.todo(
+      "should call the api with the query $query and return the expected $expected"
     );
 
-    beforeAll(() => server.listen());
-    afterEach(() => server.resetHandlers());
-    afterAll(() => server.close());
+    it.todo("should throw an error if the query is empty");
 
-    it("should return the correct result", () => {
-      const query = "test";
-      const expectedResult = mockResult(query);
-      return expect(searchUsers("test")).resolves.toEqual(expectedResult);
-    });
-
-    it.each`
-      query     | expected
-      ${"test"} | ${mockResult("test")}
-      ${"foo"}  | ${mockResult("foo")}
-      ${"bar"}  | ${mockResult("bar")}
-    `(
-      "should call the api with the query $query and return the expected $expected",
-      ({ query, expected }: { query: string; expected: UserSearchResult }) => {
-        return expect(searchUsers(query)).resolves.toEqual(expected);
-      }
-    );
-
-    it("should throw an error if the query is empty", () => {
-      return expect(searchUsers("")).rejects.toThrow();
-    });
-
-    it("should throw an error if the serve respond with the code 500", () => {
-      // override initial "GET https://api.github.com/search/users" request handler
-      server.use(
-        rest.get(`https://api.github.com/search/users`, (req, res, ctx) => {
-          return res(ctx.status(500));
-        })
-      );
-      return expect(searchUsers("test")).rejects.toThrow();
-    });
+    it.todo("should throw an error if the serve respond with the code 500");
   });
 });
 
